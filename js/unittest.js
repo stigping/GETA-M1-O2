076e263e44afdcefb4e8fe4d4cf00ec33e477ffe
Stig Omdal <stigping@gmail.com>
2020-06-20 21:58:51 +0200

    Update unittest.js

---
+++ js/unittest.js
@@ -0,0 +1,15 @@
+QUnit.test( "hello test", function( assert ) {
+    assert.equal( 1 == "1", "Passed!" );
+  });
+
+// QUnit.test( "hello test", function( assert ) {
+//   assert.ok( 1 == "1", "Passed!" );
+// });
+
+// QUnit.test( "hello test", function( assert ) {
+//     assert.ok( 1 == "1", "Passed!" );
+//   });
+
+// QUnit.test( "hello test", function( assert ) {
+//   assert.ok( 1 == "1", "Passed!" );
+// });
