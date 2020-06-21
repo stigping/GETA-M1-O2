fcdbf71d579fa832706011f731e4bc398609d6bc
Stig Omdal <stigping@gmail.com>
2020-06-21 11:56:17 +0200

    Updated model comments

--- js/model.js
+++ js/model.js
@@ -4,6 +4,6 @@ let numbers = [7, 3, 1, 5, 8]; // Default array
 let chosenBar = 'none'; // Variable for which bar is selected
 let inputValue = null; // Variable for what is written in the input field
 
-let selected = false; //
-let selectedId; //
+let selected = false; // Determines if a bar is selected or not
+let selectedId; // Holds the selected bars id
 let errMsg = ''; // Variable for the div that show an error message when an invalid number is entered in the input field
