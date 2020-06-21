e9510524ff2659432eeaa38b0cf1b231f75fcc2b
Stig Omdal <stigping@gmail.com>
2020-06-21 13:00:44 +0200

    Update unittest.js
    Changed Unit Tests #1 performed using QUnit

--- js/unittest.js
+++ js/unittest.js
@@ -1,6 +1,9 @@
-QUnit.test("Selects a bar and gets ID", function (assert)
+QUnit.test("Selects a bar and gets ID, then deselects", function (assert)
 {
 	reset();
 	selectBar("1");
     assert.deepEqual(selectedId, "1", "Selected bar 1");
+
+    selectBar("1");
+	assert.deepEqual(selectedId, "none", "Deselected bar 1");
 });
